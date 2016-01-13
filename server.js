8053301e22d358397f8e6df0aa20440f0c861e66
wandergis <763393390@qq.com>
2016-01-13 11:55:53 +0800

    add sourcemap

--- server.js
+++ server.js
@@ -4,16 +4,21 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-for (key in config.entry) {
+for(key in config.entry){
   config.entry[key].unshift('webpack-dev-server/client?http://0.0.0.0:8090', "webpack/hot/dev-server");
 }
+config.devtool='#source-map';
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 config.plugins.push(new webpack.NoErrorsPlugin());
 //http://192.168.1.6/openapi2/getregister/
 // 相当于通过本地node服务代理请求
 var proxy = [{
-  path: "/openapi/*",
-  target: "http://192.168.1.6",
+  path: "/api_function/*",
+  target: "http://192.168.1.6:8084",
+  host: "192.168.1.6"
+},{
+  path: "/apilib/*",
+  target: "http://192.168.1.6:8084",
   host: "192.168.1.6"
 }];
 //启动服务
@@ -21,7 +26,8 @@ var app = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: false,
   historyApiFallback: true,
-  proxy: proxy
+  proxy:proxy,
+  stats: { colors: true }
 });
 app.listen(8090, function(err, result) {
   if (err) {
